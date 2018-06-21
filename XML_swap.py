2aba4a09a9bbb6d875831a582a167f3023d29ea7
Yining Qian <34137359+Annoah@users.noreply.github.com>
2018-06-21 10:51:44 +0800

    Update XML_swap.py

--- XML_swap.py
+++ XML_swap.py
@@ -16,13 +16,8 @@ for xmlFile in files:  # 遍历文件夹
     dom = xml.dom.minidom.parse(os.path.join(path, xmlFile))  ###最核心的部分os.path.join(path,xmlFile),路径拼接,输入的是具体路径
     root = dom.documentElement
     # 获取标签对name/pose之间的值
-    #folder = root.getElementsByTagName('folder')
     name = root.getElementsByTagName('name')
     # 重命名class name
-    #for i in range(len(folder)):
-        #print(folder[i].firstChild.data)
-        #folder[i].firstChild.data = 'VOC2007'
-        #print(folder[i].firstChild.data)
     for i in range(len(name)):
         print(name[i].firstChild.data)
         name[i].firstChild.data = 'butterfly'
@@ -32,9 +27,3 @@ for xmlFile in files:  # 遍历文件夹
     with open(os.path.join(path, xmlFile), 'w') as fh:
         dom.writexml(fh)
         print('写入 OK!')
-
-    #with open('dom_write.xml','w',encoding='UTF-8') as fh:
-             # 4.writexml()第一个参数是目标文件对象，第二个参数是根节点的缩进格式，第三个参数是其他子节点的缩进格式，
-             # 第四个参数制定了换行格式，第五个#参数制定了xml内容的编码。
-     #   dom.writexml(fh,indent='',addindent='\t',newl='\n',encoding='UTF-8')
-      #  print('写入xml OK!')
